97f4dc8968390dfe3b114f093d93b8dd7a3b58a5
OGynning <113348004+OGynning@users.noreply.github.com>
2023-10-23 10:13:49 +0200

    Initial Commit

---
+++ Uppgift5MLcore/Uppgift5MLcoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uppgift5MLcoreApp.swift
+//  Uppgift5MLcore
+//
+//  Created by oscar gynning on 2023-10-23.
+//
+
+import SwiftUI
+
+@main
+struct Uppgift5MLcoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
